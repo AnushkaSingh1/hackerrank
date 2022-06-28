a3ffa750ca90c9efde776bef8554f4461565fa2a
Anushka Singh <anushkasingh117733@gmail.com>
2022-06-28 23:52:37 +0530

    Create week2.3.cpp

---
+++ week2.3.cpp
@@ -0,0 +1,37 @@
+long flippingBits(long n)
+{
+    return (4294967295 - n);
+}
+
+
+
+
+
+
+
+
+
+You will be given a list of 32 bit unsigned integers. Flip all the bits ( and ) and return the result as an unsigned integer.
+
+Example
+
+. We're working with 32 bits, so:
+
+
+
+Return .
+
+Function Description
+
+Complete the flippingBits function in the editor below.
+
+flippingBits has the following parameter(s):
+
+int n: an integer
+Returns 4294967286
+
+int: the unsigned decimal integer result
+Input Format
+
+The first line of the input contains q, the number of queries.
+Each of the next q lines contain an integer,n , to process.
