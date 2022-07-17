df65b7a20efd9888c413ee45f9172203081e0c60
Anushka Singh <anushkasingh117733@gmail.com>
2022-07-17 23:06:36 +0530

    Create week4.6.cpp

---
+++ week4.6.cpp
@@ -0,0 +1,111 @@
+string caesarCipher(string s, int k) {
+    
+    char res[INT16_MAX];
+    
+    k = k % 26;
+    
+    for(unsigned int i = 0; i<s.length(); ++i)
+    {
+        if(isalpha(s[i]))
+        {
+            int c = s[i];
+            
+            c = c+k;
+            
+            if(isupper(s[i]) && c>90)
+                c = c-26;
+            else if(islower(s[i]) && c>122)
+                c = c-26;
+                
+            res[i] = char(c);
+        }
+        else
+        {
+            res[i] = s[i];
+        }
+    }  
+    return res;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Julius Caesar protected his confidential information by encrypting it using a cipher. Caesar's cipher shifts each letter by a number of letters. If the shift takes you past the end of the alphabet, just rotate back to the front of the alphabet. In the case of a rotation by 3, w, x, y and z would map to z, a, b and c.
+
+Original alphabet:      abcdefghijklmnopqrstuvwxyz
+Alphabet rotated +3:    defghijklmnopqrstuvwxyzabc
+Example
+
+
+The alphabet is rotated by , matching the mapping above. The encrypted string is .
+
+Note: The cipher only encrypts letters; symbols, such as -, remain unencrypted.
+
+Function Description
+
+Complete the caesarCipher function in the editor below.
+
+caesarCipher has the following parameter(s):
+
+string s: cleartext
+int k: the alphabet rotation factor
+Returns
+
+string: the encrypted string
+Input Format
+
+The first line contains the integer, , the length of the unencrypted string.
+The second line contains the unencrypted string, .
+The third line contains , the number of letters to rotate the alphabet by.
+
+Constraints
+
+
+
+ is a valid ASCII string without any spaces.
+
+Sample Input
+
+11
+middle-Outz
+2
+Sample Output
+
+okffng-Qwvb
+Explanation
+
+Original alphabet:      abcdefghijklmnopqrstuvwxyz
+Alphabet rotated +2:    cdefghijklmnopqrstuvwxyzab
+
+m -> o
+i -> k
+d -> f
+d -> f
+l -> n
+e -> g
+-    -
+O -> Q
+u -> w
+t -> v
+z -> b
+
