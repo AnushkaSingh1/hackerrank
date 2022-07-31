8a076084137d301d956c6ebc026e6ce5262ec420
Anushka Singh <anushkasingh117733@gmail.com>
2022-07-31 22:17:05 +0530

    Create week6.8.cpp

---
+++ week6.8.cpp
@@ -0,0 +1,76 @@
+long sumXor(long n) {
+    int count=0;
+    int t=0;
+    while(n!=0)
+    {
+        if((n&1)==0) 
+            count++;
+        n=n>>1;
+    }
+    return pow(2,count);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+Given an integer , find each  such that:
+
+where  denotes the bitwise XOR operator. Return the number of 's satisfying the criteria.
+
+Example
+
+There are four values that meet the criteria:
+
+Return .
+
+Function Description
+
+Complete the sumXor function in the editor below.
+
+sumXor has the following parameter(s):
+- int n: an integer
+
+Returns
+- int: the number of values found
+
+Input Format
+
+A single integer, .
+
+Constraints
+
+Subtasks
+
+ for  of the maximum score.
+Output Format
+
+Sample Input 0
+
+5
+Sample Output 0
+
+2
+Explanation 0
+
+For , the  values  and  satisfy the conditions:
+
+Sample Input 1
+
+10
+Sample Output 1
+
+4
+Explanation 1
+
+For , the  values , , , and  satisfy the conditions:
+
